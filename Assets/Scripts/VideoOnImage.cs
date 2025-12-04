36b8f1424fab71c2b12cd4494be5f997da18820b
박민근 <agebreak@krafton.com>
2025-12-04 16:29:43 +0900

    승리/패배 동영상 추가

---
+++ Assets/Scripts/VideoOnImage.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoOnImage : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    
+    void Start()
+    {
+        // RenderTexture 생성
+        RenderTexture renderTexture = new RenderTexture(1920, 1080, 0);
+        
+        // VideoPlayer 설정
+        videoPlayer.renderMode = VideoRenderMode.RenderTexture;
+        videoPlayer.targetTexture = renderTexture;
+        
+        // RawImage에 연결
+        rawImage.texture = renderTexture;
+        
+        // 비디오 재생
+        videoPlayer.Play();
+    }
+}
