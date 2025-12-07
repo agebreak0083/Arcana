3c510d095d6afc689fd4e3c2cb8b4e93deb117ca
박민근 <agebreak@krafton.com>
2025-12-07 17:23:27 +0900

    IntroScene 추가가

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button gachaButton;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        gachaButton.onClick.AddListener(OnGachaButtonClicked);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("TacticsScene");
+    }
+
+    void OnGachaButtonClicked()
+    {
+        SceneManager.LoadScene("GachaScene");
+    }
+}
