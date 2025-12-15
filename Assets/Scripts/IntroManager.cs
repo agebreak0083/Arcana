d07864944696318f126d39894bb94d8518121073
박민근 <agebreak@krafton.com>
2025-12-15 18:27:50 +0900

    versionText 추가

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -1,15 +1,22 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
+    public TextMeshProUGUI versionText;
     public Button startButton;
     public Button gachaButton;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if(versionText != null)
+        {
+            versionText.text = "v.08";
+        }
         startButton.onClick.AddListener(OnStartButtonClicked);
         gachaButton.onClick.AddListener(OnGachaButtonClicked);
     }
