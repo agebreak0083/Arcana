1b790b73aacc9995452e60cc7e23960d0e449910
박민근 <agebreak@krafton.com>
2026-01-12 15:42:06 +0900

    V.26 업데이트

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -19,7 +19,7 @@ public class IntroManager : MonoBehaviour
     {
         if(versionText != null)
         {
-            versionText.text = "v.25";
+            versionText.text = "v.26";
         }
 
         SetId(UserDataManager.Instance.currentUserData.playerName);
